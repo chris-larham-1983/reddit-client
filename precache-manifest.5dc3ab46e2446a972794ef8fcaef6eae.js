561408cf7e6f0e57ea003bb035c12e64e21a2ef5
chris-larham-1983 <chrislarham@outlook.com>
2021-12-07 15:48:58 +0000

    Updates

---
+++ precache-manifest.5dc3ab46e2446a972794ef8fcaef6eae.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f92c6fd25b67123e5ade1631d88bdd",
+    "url": "/reddit-client/index.html"
+  },
+  {
+    "revision": "a49b80da2eeb6805354e",
+    "url": "/reddit-client/static/css/main.c8402964.chunk.css"
+  },
+  {
+    "revision": "c261bd2e2220653548e0",
+    "url": "/reddit-client/static/js/2.b5ad368a.chunk.js"
+  },
+  {
+    "revision": "f09bafd8b2e7fe4d8e763cfbd1fdf23b",
+    "url": "/reddit-client/static/js/2.b5ad368a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a49b80da2eeb6805354e",
+    "url": "/reddit-client/static/js/main.fd3eded5.chunk.js"
+  },
+  {
+    "revision": "2187781238032cce2224",
+    "url": "/reddit-client/static/js/runtime-main.5a52a129.js"
+  },
+  {
+    "revision": "88cf4776e0fd1431175b52a33e1343af",
+    "url": "/reddit-client/static/media/coding.88cf4776.png"
+  },
+  {
+    "revision": "c0b2179c5328ccff9473ce463ed02c57",
+    "url": "/reddit-client/static/media/conspiracy.c0b2179c.png"
+  },
+  {
+    "revision": "ad5b875f4943a355066e2a511fb34f7c",
+    "url": "/reddit-client/static/media/coronavirus.ad5b875f.png"
+  },
+  {
+    "revision": "2a95690563b8ed771f69a54773e4e6b1",
+    "url": "/reddit-client/static/media/darwin.2a956905.png"
+  },
+  {
+    "revision": "3940f8633a6c911a7283f786d0878f34",
+    "url": "/reddit-client/static/media/religion.3940f863.png"
+  },
+  {
+    "revision": "51e466154d59edac5a89f4f251632fb1",
+    "url": "/reddit-client/static/media/search.51e46615.png"
+  },
+  {
+    "revision": "10517fb2236bd4f4f328be008334f3dc",
+    "url": "/reddit-client/static/media/soccer.10517fb2.png"
+  }
+]);
